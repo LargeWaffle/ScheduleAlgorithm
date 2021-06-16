46c7e62b074c6991d9f0c44820b7753ea9e0f2f6
LargeWaffle <sirventhomas@gmail.com>
2021-06-16 11:31:51 +0200

    Bash script v2

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+CURRENTDATE=$(date +"%d/%m/%Y %T")
+FILE=projet
+
+echo "IT45 Building task"
+echo -e "Time - ${CURRENTDATE}\n"
+
+if test -f "$FILE"; then
+  echo "File 'project' already exists. File overwritten"
+  eval rm projet
+fi
+
+echo "Generating executable..."
 eval make
--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-eval ./projet
+
+CURRENTDATE=$(date +"%d/%m/%Y %T")
+FILE=projet
+
+echo "IT45 Running task"
+echo -e "Time - ${CURRENTDATE}\n"
+
+if [ ! -f "$FILE" ]; then
+  echo -e "File 'project' does not exits\nRun the building task beforehand !"
+else
+  eval ./projet
+fi
+
